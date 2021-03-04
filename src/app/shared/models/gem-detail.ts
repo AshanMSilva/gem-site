e97e7f4d1975eb01140bd5b2b1caf2b3d6f4943c
Ashan M Silva <47840176+AshanMSilva@users.noreply.github.com>
2021-03-04 21:33:53 +0530

    name changed

---
+++ src/app/shared/models/gem-detail.ts
@@ -0,0 +1,25 @@
+class GemDetail{
+    type:string;
+    id:string;
+    date: string;
+    sgtlReportNumber:string;
+    image:string;
+    weight:string;
+    shape:string;
+    transparency:string;
+    dimensions:string;
+    refractiveIndex:string;
+    specifyGravity:string;
+    hardness:string;
+    opticCharacter:string;
+    magnification:string;
+    color:string;
+    species:string;
+    variety:string;
+    comments:string;
+    apex:string;
+    qrCode:string;
+    
+
+
+}
--- src/app/shared/models/gem-report.ts
+++
@@ -1,3 +0,0 @@
-class GemReport{
-    id:string;
-}
