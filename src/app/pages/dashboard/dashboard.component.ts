09ed0e551d9a89bca1ecc05b833feec78f7e137a
Ashan M Silva <47840176+AshanMSilva@users.noreply.github.com>
2021-03-04 12:38:20 +0530

    move dashboard to pages

---
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -0,0 +1,23 @@
+import { Route } from '@angular/compiler/src/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import * as Chartist from 'chartist';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+export class DashboardComponent implements OnInit {
+
+  constructor(
+    private router: Router
+  ) { }
+  ngOnInit() {
+
+  }
+  goToNewGemReport(){
+    this.router.navigateByUrl('gem-report/new');
+  }
+
+}
