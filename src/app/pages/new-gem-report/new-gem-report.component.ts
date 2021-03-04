39e3b7daaf6d18cfc44c0d7bb15e3a6f7aa8f2eb
Ashan M Silva <47840176+AshanMSilva@users.noreply.github.com>
2021-03-04 12:38:54 +0530

    create new gem report component

---
+++ src/app/pages/new-gem-report/new-gem-report.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-gem-report',
+  templateUrl: './new-gem-report.component.html',
+  styleUrls: ['./new-gem-report.component.css']
+})
+export class NewGemReportComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
