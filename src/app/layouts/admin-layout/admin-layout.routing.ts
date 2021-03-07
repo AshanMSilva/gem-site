27c96e411878fe5ccb41d44b5a819f4c4a9fedab
Ashan M Silva <47840176+AshanMSilva@users.noreply.github.com>
2021-03-07 16:47:06 +0530

    Update admin-layout.routing.ts

--- src/app/layouts/admin-layout/admin-layout.routing.ts
+++ src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -4,7 +4,7 @@ const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(['login']);
 import { UserProfileComponent } from '../../user-profile/user-profile.component';
 import { TableListComponent } from '../../table-list/table-list.component';
 import { TypographyComponent } from '../../typography/typography.component';
-import { IconsComponent } from '../../icons/icons.component';
+
 import { MapsComponent } from '../../maps/maps.component';
 import { NotificationsComponent } from '../../notifications/notifications.component';
 import { UpgradeComponent } from '../../upgrade/upgrade.component';
