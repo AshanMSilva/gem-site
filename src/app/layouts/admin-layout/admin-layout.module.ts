3a9cfb1ea4d88d51b2afd469a74aed4ab66cc02c
Ashan M Silva <47840176+AshanMSilva@users.noreply.github.com>
2021-03-07 16:47:03 +0530

    Update admin-layout.module.ts

--- src/app/layouts/admin-layout/admin-layout.module.ts
+++ src/app/layouts/admin-layout/admin-layout.module.ts
@@ -6,7 +6,7 @@ import { AdminLayoutRoutes } from './admin-layout.routing';
 import { UserProfileComponent } from '../../user-profile/user-profile.component';
 import { TableListComponent } from '../../table-list/table-list.component';
 import { TypographyComponent } from '../../typography/typography.component';
-import { IconsComponent } from '../../icons/icons.component';
+
 import { MapsComponent } from '../../maps/maps.component';
 import { NotificationsComponent } from '../../notifications/notifications.component';
 import { UpgradeComponent } from '../../upgrade/upgrade.component';
@@ -49,7 +49,7 @@ import { PdfViewComponent } from 'app/pages/pdf-view/pdf-view.component';
     UserProfileComponent,
     TableListComponent,
     TypographyComponent,
-    IconsComponent,
+ 
     MapsComponent,
     NotificationsComponent,
     UpgradeComponent,
